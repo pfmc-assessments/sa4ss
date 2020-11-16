bb701e5e95a753e7338da34715cfcb5da3669410
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-11-16 05:02:07 -0800

    feat(doit): automatically deal with multiple tar files
    Now, it takes the most recent version number only, this way the
    version number doesn't have to constantly be changed in the function.
    
    Can give it authors now as an argument.
    
    Sets the working directory back to the original getwd().

--- R/doit.R
+++ R/doit.R
@@ -1,13 +1,28 @@
-doit <- function (dirgit = "c:/stockAssessment/ss/sa4ss") {
+#' Internal Code to Build, Load, and Render the sa Template
+#'
+#' A non-exported function used by Kelli F. Johnson to
+#' build and load \pkg{sa4ss} and copy and render the template
+#' for a stock assessment document.
+#'
+#' @param dirgit A file path to where the sa4ss repository is cloned.
+#' @template authors
+#' @author Kelli Faye Johnson
+#'
+doit <- function (dirgit = "c:/stockAssessment/ss/sa4ss",
+  authors = c("Kelli F. Johnson", "Chantel R. Wetzel", "Joseph J. Bizzarro")) {
+  oldwd <- getwd()
+  on.exit(setwd(oldwd), add = TRUE)
   setwd(dirgit)
   build()
   if ("package:sa4ss" %in% search()) unload(package = "sa4ss")
-  install.packages(file.path(dirname(dirgit), "sa4ss_0.0.0.9001.tar.gz"),
+  install.packages(tail(n = 1,
+    dir(dirname(dirgit), pattern = "sa4ss_[0-9\\.]+\\.tar.gz",
+      full.names = TRUE)),
     type = "source")
   library(sa4ss)
   unlink("doc", recursive = TRUE)
   sa4ss::draft(
-    authors = c("Kelli F. Johnson", "Chantel R. Wetzel", "Joeseph Bizzarro"),
+    authors = authors,
     create_dir = TRUE)
   setwd("doc")
   bookdown::render_book("00a.Rmd", clean = FALSE)
