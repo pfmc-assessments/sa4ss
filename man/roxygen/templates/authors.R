785239d79942b0fd143f969968b34f91ef6e3c8e
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-11-16 05:29:07 -0800

    fix(authors): spelling and add missing
    Increase the documentation, change the spelling of a few authors that
    were wrong, and add missing authors.

--- man/roxygen/templates/authors.R
+++ man/roxygen/templates/authors.R
@@ -1,3 +1,9 @@
 #' @param authors A vector of author names. For example,
 #' \code{"John R. Doe"} without first name first. The middle
-#' initial or name is optional.
+#' initial or name is optional. Entries must exactly match
+#' those used in the \code{data-raw\authors.csv} file.
+#' If you need additional authors added to this file
+#' please contact the package maintainers or add them to the
+#' \code{.csv} file and run the R script also found in that
+#' folder prior to submitting a pull request. Following this
+#' workflow ensures that the author's address is also added.
