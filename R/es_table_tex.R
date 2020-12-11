2c88b039d002ffbcc2a2da369e9d42b42506fa38
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-12-11 06:57:28 -0800

    fix: remove dplyr and replace with general fxn

--- R/es_table_tex.R
+++ R/es_table_tex.R
@@ -4,41 +4,37 @@
 #' @param table_folder 
 #' 
 #' @author Chantel Wetzel
-#' @importFrom dplyr mutate_if
-
+#' 
 #' 
 es_table_tex <- function(mod_loc, 
 					  table_folder = 'tables'){
+
+	# Function to round data-frame with characters and numeric values
+	round_df <- function(df, digits) {
+  		nums <- vapply(df, is.numeric, FUN.VALUE = logical(1))
+		df[,nums] <- round(df[,nums], digits = digits)
+		(df)
+	}
 
 	df = read.csv(file.path(mod_loc, table_folder, "table_labels.csv"))
 
 	for(i in 1:length(df$filename)){
 
-		tab = read.csv(file.path(mod_loc, table_folder, df$filename[i]),
-					   check.names = FALSE)
+		tab = read.csv(file.path(mod_loc, table_folder, df$filename[i]), check.names = FALSE)
 		tex_name = sub(pattern = ".csv", '', df$filename[i])
-
-		#col_names = gsub("\\.", " ", colnames(tab))
 		col_names = gsub("\\_", " ", colnames(tab))
-		# If there are repeat column names a number is added in the csv
-		# The following line removes the trailing numbers hopefully
-		#col_names = gsub("[0-9]$", "", col_names)
-		
 		n = ncol(tab) - 1
 
 		if(col_names[1] == "Year"){ 
-			dig = 2
 			t <- table_format(x = as.data.frame(tab),
 			                  caption = df$caption[i],
 			                  label = df$label[i],
-			                  digits = c(0, rep(dig, n)),
+			                  digits = c(0, rep(2, n)),
 			                  longtable = TRUE,
 			                  col_names = col_names,
 			                  align = c('r',rep('c', n)))
 		}else{
-			tab <- data.frame(tab)
-			tab <- tab %>% dplyr::mutate_if(is.numeric, round, digits = 2)
-			t <- table_format(x = as.data.frame(tab),
+			t <- table_format(x = round_df(df = data.frame(tab), digits = 2),
 			                  caption = df$caption[i],
 			                  label = df$label[i],
 			                  longtable = TRUE,
