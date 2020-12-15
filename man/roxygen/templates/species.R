c06dc43cb7e881c964f364fa0a842ccb067e2568
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2020-12-15 14:28:36 -0800

    v0.0.0.9006 Start table compliance

--- man/roxygen/templates/species.R
+++ man/roxygen/templates/species.R
@@ -1,5 +1,10 @@
-#' @param species A character string specifying the species name.
-#' Please capitilize as you would like to see it used in the middle
-#' of a sentence because this will not be altered. For example,
-#' \code{"Pacific hake"} is used in the stock assessment of hake;
-#' not \code{"pacific hake"}.
+#' @param species A vector of character strings providing species names for
+#' all species included in the stock assessment. Typically, there will only
+#' be one species, but this template can accommodate a species complex as well.
+#' Each species should be capitalized as you would like to see it used in the middle
+#' of a sentence. For example,
+#' \code{"Pacific Hake"} is used in the stock assessment of hake;
+#' not \code{"pacific hake"} because Pacific Hake is the preferred name, not
+#' pacific hake. The text can be called in the document using spp for the
+#' form to be used in the middle of a sentence and Spp when placed at the
+#' beginning of a sentence.
