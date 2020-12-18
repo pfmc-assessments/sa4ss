07188bc37eeb6f874e30175ef6e246a3b1bd27af
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-12-18 07:35:49 -0800

    refactor: general fxn and input to be able to use on any plotinfo csv
    These changes were meant to make this function more adaptable allowing it to be applied to create tex table for other csv files that will be added to the document.

--- R/es_table_tex.R
+++ R/es_table_tex.R
@@ -1,20 +1,38 @@
 #' Create tex file from executive summary tables
 #'
-#' @template mod_loc
+#' @param dir The directory to look for the csv file that has a list of all
+#' csv files to create tex files for. The csv file format should match
+#' the table_labels.csv created by r4ss function SSexecutivesummary. The columns 
+#' names in this folder are caption, altcaption, label, filename, and loc (optional). 
+#' The loc column is a directory location to find the file (filename) that 
+#' a tex file should be created for. The \code{dir} can be set to the 
+#' \code{mod_loc} where the function will automatically look for them inside a 
+#' table folder in that location.  
 #' @param table_folder The relative path for the table directory, where it
-#' must be relative to \code{mod_loc} because it will be appended to this
+#' must be relative to \code{dir} because it will be appended to this
 #' argument using \code{file.path}. An alternative is to supply the full
-#' file path in \code{mod_loc} and use \code{table_folder = ""}.
+#' file path in dir and use \code{table_folder = ""}.
 #' @param save_table optional input that requires a full path which will 
 #' allow users to save the tex file to a specific location (e.g. inside doc folder)
-#' other than the \code{file.path(mod_loc, table_folder)}. Default NULL
+#' other than the \code{file.path(dir, table_folder)}. Default NULL
 #'
 #' @author Chantel Wetzel
 #' 
+#' @examples
+#' \dontrun{
+#'	#example of creating tex files for those created by the 
+#'  # r4ss function SSexecutivesummary
+#' 	es_table_tex(dir = mod_loc)
+#'	#example of using a user created csv file:
+#'	es_table_tex(dir = "C:/models/table_to_add_to_doc",
+#'				 save_loc = "C:/model/tex_tables",
+#'				 csv_name = "non_es_document_tables.csv")
+#' }			
 #' 
-es_table_tex <- function(mod_loc, 
-					  table_folder = 'tables',
-					  save_loc = NULL){
+es_table_tex <- function(dir, 
+					     table_folder = "",
+					     save_loc = NULL,
+					     csv_name = "table_labels.csv"){
 
 	# Function to round data-frame with characters and numeric values
 	round_df <- function(df, digits) {
@@ -23,15 +41,27 @@ es_table_tex <- function(mod_loc,
 		(df)
 	}
 
-	df = utils::read.csv(file.path(mod_loc, table_folder, "table_labels.csv"))
+	if (missing(table_folder)){
+		if(dir.exists(file.path(dir, "tables"))){
+			df = utils::read.csv(file.path(dir, "tables", csv_name))
+		}else{
+			df = utils::read.csv(file.path(dir, csv_name))			
+		}
+	} else {
+		df = utils::read.csv(file.path(dir, table_folder, csv_name))
+	}
 
 	if (is.null(save_loc)) {
-		save_loc = file.path(mod_loc, table_folder) }
+		save_loc = file.path(dir, table_folder) 
+	}
 	
-
 	for(i in 1:length(df$filename)){
+		if("loc" %in% colnames(df)){
+			tab = utils::read.csv(file.path(df$loc[i], df$filename[i]), check.names = FALSE)
+		} else {
+			tab = utils::read.csv(file.path(dir, table_folder, df$filename[i]), check.names = FALSE)
+		}
 
-		tab = utils::read.csv(file.path(mod_loc, table_folder, df$filename[i]), check.names = FALSE)
 		tex_name = sub(pattern = ".csv", '', df$filename[i])
 		col_names = gsub("\\_", " ", colnames(tab))
 		n = ncol(tab) - 1
