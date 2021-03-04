bcf11d129437a6175d5b3992d7e402c3b30461ed
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-03-04 11:45:57 -0800

    doc(add_figure): spacing, title case of add_figure documentation

--- R/add_figure.R
+++ R/add_figure.R
@@ -1,16 +1,24 @@
 #' Read, render, and use a figure from a specific directory.
-#' This function concatinates input of a specified figure with 
+#'
+#' Concatenate input regarding a specified figure with
 #' the caption, alternative caption for accessibility, and
 #' reference label.
 #'
 #' @param filein The path of the figure to be added
 #' (e.g., "C:\\My figure directory\\plot.png").
-#' @param caption figure caption to add below the figure in the document
-#' @param alt_caption Default NULL. Alternative figure caption for accessibility.
-#' If not specified the caption will be used.
-#' @param label figure reference for citation of figure in the document
-#' @param width figure width in terms of percentage of size. Default 100
-#' @param height figure height in terms of perccentage of size. Default 100
+#' @param caption A character string providing the figure caption that will be
+#' added below the figure in the document.
+#' A default text string is provided, but it is not informative and should be changed.
+#' @param alt_caption A character string providing alternative text for the figure.
+#' The default is `NULL`, which will force the alternative text to be equal to `caption`.
+#' Note, that the default is not ideal. Instead, alternative text that describes what
+#' is going on in the figure should be included.
+#' @param label A character string that will be used as the figure reference
+#' for citation of figure in the document.
+#' @param width A numeric value between 0 and 100 that dictates the figure width
+#' in terms of percentage of size. The default is 100.
+#' @param height A numeric value between 0 and 100 that dictates the figure height
+#' in terms of percentage of size. The default is 100.
 #'
 #' @author Chantel R. Wetzel
 #' @export
@@ -26,18 +34,25 @@
 #' 
 #' ```{r, results = 'asis'}
 #' add_figure(filein = file.path("My figure directory", "plots", "ts7_Spawning_output.png")
-#' 			  caption = "Spawning output timeseries.", 
-#'			  alt_caption = NULL,
-#' 			  label = "ssb",
-#'			  width = 100, height = 100)
+#'   caption = "Spawning output timeseries.",
+#'   alt_caption = NULL,
+#'   label = "ssb",
+#'   width = 100, height = 100
+#' )
 #' ```
 #' }
 #'
-add_figure <- function(filein, caption = "Add figure caption", alt_caption = NULL, 
-					   label = 'default', width = 100, height = 100) {
+add_figure <- function(
+  filein,
+  caption = "Add figure caption",
+  alt_caption = NULL,
+  label = "default",
+  width = 100,
+  height = 100
+) {
 
   if (is.null(alt_caption)) { alt_caption = caption }
-  
+
   cat('\n![',caption,'.\\label{fig:',label,'}](',filein,'){width=',width,'% height=',height,'% alt="',alt_caption,'."}\n',sep='')
 
 }
