c33d4af62a4377e9224aa3ae95a1273f1f90e3bd
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-03-05 04:38:24 -0800

    Add Maia and Lee Qi to author, update docs for author data

--- R/authors.R
+++ R/authors.R
@@ -1,8 +1,17 @@
 #' A data frame of authors and their affiliations
-#' 
+#'
 #' A data frame of potential stock assessment authors and their
 #' affiliations.
-#' 
+#'
+#' @details
+#' There are multiple ways to add your name to the list of authors.
+#' You could fork the package,
+#' add your name to the csv file `authors.csv` which is located in the `data-raw` directory, and
+#' submit a pull request.
+#' You could also just email the package maintainer your name, affiliation, and address.
+#' Either option is great.
+#' Just remember to follow the steps again when any of your information changes.
+#'
 #' @name authors
 #' @docType data
 #' @keywords data
